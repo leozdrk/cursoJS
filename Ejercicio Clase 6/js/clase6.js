3ceab0fa15665aa9bd1b3f2d286cf2541416381d
alumno <alumno@utnfra.com.ar>
2019-10-09 16:59:24 -0300

    un comentario

--- Ejercicio Clase 6/js/clase6.js
+++ Ejercicio Clase 6/js/clase6.js
@@ -32,6 +32,18 @@ function CargarVariables()
 
   let body = document.body;
   let ultimoHijo = body.lastElementChild; //Trae al ultimo hijo del body
+
+  // Creo un elemento del tipo enlace
+  let enlace = document.createElement('a');
+  // Creo un nodo del tipo texto
+  let texto = document.createTextNode('Ir a Clarin');
+  // Agrego al nodo texto como hijo del elemento enlace
+  enlace.appendChild(texto);
+  // Seteo los atributos del elemento enlace
+  enlace.setAttribute('href',"http://clarin.com");
+  enlace.setAttribute('target','_blank');
+  //Agrego en el body el nuevo elemento enlace
+  body.appendChild(enlace);
 }
 
 //Ctrl + k + c Cometa lo pintado, para descomentar ctrl + k + u
